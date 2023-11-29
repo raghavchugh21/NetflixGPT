715aa8c170409465c13e0dd2f517769f52771a62
Raghav Chugh <raghavchughcode@gmail.com>
2023-11-29 23:26:13 +0530

    adding auth component

---
+++ netflix-gpt/src/components/Auth.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+function Auth() {
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <div className="flex h-screen w-screen justify-center items-center">
+
+      <div className="auth-banner h-screen w-screen brightness-50 absolute top-0 -z-10"></div>
+      <div className="header w-screen absolute top-0">
+        <a className="logo block h-[55px] w-[220px] relative top-5 left-8" href="/"></a>
+      </div>
+
+      <div className="flex flex-col justify-center h-[600px] w-[450px] px-[68px] py-[70px] bg-black bg-opacity-60 rounded-2xl">
+        
+        <span className="h-[50px] text-4xl text-white mb-5">{isLogin ? "Sign In" : "Sign Up"}</span>
+
+        {!isLogin && <input className="h-[50px] rounded-md mb-5 bg-gray-300 pl-2" type="text" id="name" placeholder="Name" required />}
+        <input className="h-[50px] rounded-md mb-5 bg-gray-300 pl-2" type="email" id="email" placeholder="Email" required />
+        <input className="h-[50px] rounded-md mb-5 bg-gray-300 pl-2" type="password" id="password" placeholder="Password" required />
+        {!isLogin && <input className="h-[50px] rounded-md mb-10 bg-gray-300 pl-2" type="password" id="confirm-password" placeholder="Confirm password" required />}
+
+        <button className="h-[50px] rounded-md bg-nflix-red text-white mb-3" type="submit">{isLogin ? "Sign In" : "Sign Up"}</button>
+        
+        { 
+          isLogin &&
+          <div className="flex w-full">
+            <input className="mr-2" type="checkbox" id="remember" name="remember"/>
+            <label className="text-gray-400 text-sm" htmlFor="remember">
+              Remember me
+            </label>
+            <a href="/login-help" className="text-gray-400 text-sm ml-auto">
+              Need help?
+            </a>
+          </div> 
+        }
+
+        <div className="mt-auto w-full">
+        { 
+          isLogin ? 
+          <>
+            <span className="text-gray-400 text-sm mr-3">New to Netflix?</span>
+            <a onClick={() => {setIsLogin(false);}} className="text-white text-sm hover:cursor-pointer">Sign Up now.</a>
+          </>
+          :
+          <>
+            <span className="text-gray-400 text-sm mr-3">Already a user?</span>
+            <a onClick={() => {setIsLogin(true);}} className="text-white text-sm hover:cursor-pointer">Log In here.</a>
+          </>
+        }
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Auth;
--- netflix-gpt/src/index.js
+++ netflix-gpt/src/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './Login';
+import Auth from './components/Auth';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" exact element={<App/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Auth/>} />
       </Routes>
     </Router>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
